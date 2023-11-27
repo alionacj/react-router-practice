eca27f4dfba23fe2eb9248ba1ced702f79b58591
Leo Johnson <leochinonejohnson@gmail.com>
2023-11-27 16:22:27 -0600

    created navbar component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,25 @@
 import axios from 'axios';
+import { HashRouter as Router, Route } from 'react-router-dom/cjs/react-router-dom.min.js';
 import './App.css';
 import StudentForm from '../StudentForm/StudentForm.jsx';
 import StudentList from '../StudentList/StudentList.jsx';
+import Nav from '../Nav/Nav.jsx';
+
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-            <h1 className="App-title">GitHub Student List</h1>
-            </header>
-            <br/>
-            <StudentForm />
+            <Router>
+                <header className="App-header">
+                <h1 className="App-title">GitHub Student List</h1>
+                </header>
+                <Nav />
+                <br/>
+                <StudentForm />
 
-            <p>Student list:</p>
-            <StudentList />
+                <p>Student list:</p>
+                <StudentList />
+            </Router>
         </div>
     );
 }
